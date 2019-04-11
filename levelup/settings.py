114979f474213c3636c395aa51b74199ee222bc0
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-04-11 19:43:29 +0000

    New heroku build

--- levelup/settings.py
+++ levelup/settings.py
@@ -156,6 +156,7 @@ STATIC_URL = '/static/'
 
 # Configure Django App for Heroku.
 # Try to import django-heroku depending on Travis or Heroku
+# This prevents an error from occuring on Travis
 try:
     # Configure Django App for Heroku.
     import django_heroku
