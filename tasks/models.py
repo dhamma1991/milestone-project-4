f17a2a0ab9707ecdf5fd706f64759b268bd246c4
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-04-11 16:43:48 +0000

    Created done_status model field, applied migrations

--- tasks/models.py
+++ tasks/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Task(models.Model):
     task_name = models.CharField(max_length=70)
     task_notes = models.TextField(max_length=400, blank=True)
+    done_status = models.BooleanField(default=False)
     created_date = models.DateTimeField(editable=False, default=timezone.now())
     
     def __str__(self):
