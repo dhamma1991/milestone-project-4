4bc63173041f6fce85ffa1fc616aedabd747d6f2
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-04-18 02:40:31 +0000

    Added 100 levels to level_system, dummy xp values currently

--- level_system/models.py
+++ level_system/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class UserLevel(models.Model):
     level_rank = models.IntegerField()
-    xp_threshold = models.IntegerField()
+    xp_threshold = models.IntegerField(default = 100)
