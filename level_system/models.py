7d4f9067a61d9d5c08fd47edd67dc4e3bd6a042c
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-04-18 02:43:21 +0000

    Defined string self in UserLevel model

--- level_system/models.py
+++ level_system/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class UserLevel(models.Model):
     level_rank = models.IntegerField()
     xp_threshold = models.IntegerField(default = 100)
+    
+    def __str__(self):
+        # Return how the class should be displayed
+        return 'Level %s' % self.level_rank
