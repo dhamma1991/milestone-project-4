c4c7b1471c6253363796113e98626817289a1641
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-04-18 03:18:25 +0000

    Added commented out command used to populate UserLevel model

--- level_system/models.py
+++ level_system/models.py
@@ -7,3 +7,9 @@ class UserLevel(models.Model):
     def __str__(self):
         # Return how the class should be displayed
         return 'Level %s' % self.level_rank
+        
+
+# # # The below commented-out line was used in the Django shell to populate the
+# # # UserLevel database with levels and xp_threshold values
+# # # Kept here for reference
+# UserLevel.objects.bulk_create([UserLevel(level_rank = level, xp_threshold = level * 100) for level in range(1, 101)])
