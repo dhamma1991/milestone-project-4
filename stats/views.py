0d47d086da0914a46e2ca08114211dc3483798bb
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-04-20 17:31:50 +0000

    Displaying total number of tasks and total number of users on stats.html

--- stats/views.py
+++ stats/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from tasks.models import Task
 
 def get_stats(request):
+    # Count all tasks currently in the database
+    all_tasks = Task.objects.all().count()
+    # Count all the users
+    all_users = User.objects.all().count()
+    
     context = {
-        
+       'all_tasks': all_tasks, 
+       'all_users': all_users,
     }
     return render(request, 'stats/stats.html', context)
