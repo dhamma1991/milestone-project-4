54dce858e250dab7db209ca42b9ce20f5b25af8a
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-04-20 17:13:28 +0000

    Changed tasks url to point towards /tasks and not /tasks/tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'tasks'
 urlpatterns = [
-    path('tasks/', views.get_tasks, name='get_tasks'),
+    path('', views.get_tasks, name='get_tasks'),
     path('<int:task_id>/', views.detail, name='detail'),
     path('create_task', views.create_task, name='create_task'),
     path('tasks/done/<int:task_id>/<task_difficulty>/', views.toggle_done_status, name='toggle_done_status'),
