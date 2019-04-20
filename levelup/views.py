b406ce170ccc1015b635922a443bf2e76e71a65e
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-04-20 17:12:00 +0000

    Added docstring

--- levelup/views.py
+++ levelup/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 def index(request):
+    """
+    Render index.html
+    """
     return render(request, 'index.html')
