4a3e7d9d1accd4ff4e89309eddcf32111fd82850
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-05-03 12:33:20 +0000

    Added comments

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,21 +1,5 @@
-"""levelup URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-# Import Django authorization views
-from django.contrib.auth import views as auth_views
+from django.contrib import admin # Import admin to allow access to admin panel
+from django.contrib.auth import views as auth_views # Import Django authorization views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,20 +8,27 @@ from accounts import views as accounts_views
 from stats import views as stats_views
 
 urlpatterns = [
+    # Default url, take user to index page
     path('', views.index, name='index'),
+    # Admin url
     path('admin/', admin.site.urls),
+    # Tasks url, get the user's tasks (or prompt them to login)
     path('tasks/', include('tasks.urls')),
+    # Stats url, go to the stats page
     path('stats/', include('stats.urls')),
+    # Donate url, go to the donate page
     path('donate/', views.donate, name='donate'),
+    # Make a 'charge' to the user who goes through with a donation
     path('charge/', views.charge, name='charge'),
-    # Since the accounts app path comes before the auth accounts path,
-    # the accounts app path will be priority
+    # Sign up url, go to the sign up page, post the form
     path('sign_up/', accounts_views.register, name='sign_up'),
+    # Profile url, go to the user's profile page
     path('profile/', accounts_views.profile, name='profile'),
-    # URL for login, point Django towards the template in the accounts app
-    # using template_name
+    # Login url, default Django login, point Django towards the template in the accounts app
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    # Logout url, default Django logout, point Django towards the template
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    # Password reset urls, these are Django defaults
     path('password-reset/', 
         auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), 
         name='password_reset'),
@@ -50,6 +41,7 @@ urlpatterns = [
     path('password-reset-complete/', 
         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), 
         name='password_reset_complete'),
+    # /Password reset urls
     
 ]
 
