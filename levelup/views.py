b21f53a6bae5ba913cc3422d928912103dad711d
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-05-03 12:25:25 +0000

    Added comments

--- levelup/views.py
+++ levelup/views.py
@@ -1,16 +1,27 @@
 from django.http import Http404
 from django.http import HttpResponse
 from django.conf import settings
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect # Import necessary Django components
 
-import stripe
+import stripe # Required for Stripe integration
 
+"""
+This file contains views that are related to the smaller elements of functionality within the project that do not justify their own Django app
+Contained here are:
+1. The index view
+2. The donate view
+3. The charge view 
+"""
+
+""" 1. The index view """
 def index(request):
     """
     Render index.html
     """
     return render(request, 'index.html')
-    
+""" /1. The index view """
+
+""" 2. The donate view """
 def donate(request):
     """
     Render the donation page and pass along the key required by Stripe
@@ -19,7 +30,9 @@ def donate(request):
         'key': settings.STRIPE_PUBLISHABLE_KEY
     }
     return render(request, 'donate.html', context)
-    
+""" /2. The donate view """
+
+""" 3. The charge view """    
 def charge(request):
     stripe.api_key = settings.STRIPE_SECRET_KEY
     if request.method == 'POST':
@@ -30,3 +43,4 @@ def charge(request):
             source=request.POST['stripeToken']
         )
         return render(request, 'charge.html')
+""" /3. The charge view """    
