21c8bf9257bf403a8562266f5c19c55bc5724f51
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-05-12 16:23:07 +0000

    Commented out old method of resizing user uploaded profile pictures

--- accounts/models.py
+++ accounts/models.py
@@ -33,19 +33,18 @@ class Profile(models.Model):
         return '%s Profile' % self.account.username
         
     
-    # Override the save method
-    def save(self, *args, **kwargs):
-        # Run the save method of the parent class
-        super().save(*args, **kwargs)
-
-        # Open the image of the current instance
-        img = Image.open(self.image.path)
+    # # Override the save method
+    # def save(self, *args, **kwargs):
+    #     # Run the save method of the parent class
+    #     super().save(*args, **kwargs)
+    #     # Open the image of the current instance
+    #     img = Image.open(self.image.path)
         
-        # If the image is larger than the site's required dimensions
-        if img.height > 150 or img.width > 150:
-            # Specify the values the image will be resized to
-            output_size = (150, 150)
-            # Resize the image, this will keep the image's proportions
-            img.thumbnail(output_size)
-            # Save
-            img.save(self.image.path)
+    #     # If the image is larger than the site's required dimensions
+    #     if img.height > 150 or img.width > 150:
+    #         # Specify the values the image will be resized to
+    #         output_size = (150, 150)
+    #         # Resize the image, this will keep the image's proportions
+    #         img.thumbnail(output_size)
+    #         # Save
+    #         img.save(self.image.path)
