8ddeeeb5a32dd08b03f3672a195f515439cccd3e
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-05-12 15:24:22 +0000

    Created StatsModel and registered in admin panel

--- stats/models.py
+++ stats/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class StatsModel(models.Model):
+    total_xp_gain = models.IntegerField()
+    total_levels_gain = models.IntegerField()
+    stats_name = models.CharField(max_length = 10)
--- tasks/admin.py
+++ tasks/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from .models import Task
 
-# Register your models here.
-
 # Register the admin class with the model
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    # Display the created date in the admin panel
+    # Display the created date in the admin panel as read only
     readonly_fields = ('created_date',)
