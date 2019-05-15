449e500c0482c4f91fad752b1b90b7d0c8a038a0
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-05-15 16:00:28 +0000

    Fixed issue with user's xp not being correctly calculated if the user has 0 xp when they mark a task as not done

--- tasks/views.py
+++ tasks/views.py
@@ -261,13 +261,31 @@ def toggle_done_status(request, task_id, task_difficulty):
             # Calculate any 'leftover' xp
             if user.exp_points >= user.xp_threshold:
                 leftover = user.exp_points - user.xp_threshold
+                
+            # Then check the user's xp has reached or exceeded their current xp_threshold
+            if user.exp_points >= user.xp_threshold:
+                # Reset experience points, but allow the user to retain full xp from a task
+                # e.g. if a user completes an amibitous task (40xp) when they only need 10 to level up
+                # they will have 30xp towards the next level
+                user.exp_points = 0 + leftover
+                # Increment the users level by 1
+                user.level_rank +=1
+                # Increment the total levels gained on the app by 1
+                stats.total_levels_gain += 1
+                # Set the new xp_threshold
+                # Each subsequent level gets harder to obtain!
+                user.xp_threshold += base_xp_threshold
+                
+                # Feedback to the user
+                messages.success(request, "Well done! You've just gained a level!")
             
         # Else, they lose xp. This is here in case a user mistakingly marks a task as done
         # Honesty is key but monitoring the user's activities is beyond the scope of this app!
         else:
-            # Get the current user's xp before any calculations are made
-            # in case they lose a level
+            # Grab the user's current_xp in case the user ends up losing a level
             current_xp = user.exp_points
+            
+            # Now perform the xp deduction    
             # Minus the xp value of the task from their current xp
             user.exp_points -= xp
             
@@ -278,6 +296,7 @@ def toggle_done_status(request, task_id, task_difficulty):
             messages.warning(request, 'You lost {} xp'.format(xp))
             
             # If the user now has less than 0 xp
+            # This means they have levelled down
             if user.exp_points < 0:
                 # Minus a level
                 user.level_rank -= 1
@@ -292,28 +311,14 @@ def toggle_done_status(request, task_id, task_difficulty):
                 # at the previous level before marking the task as done
                 # e.g. a user on 20xp at level 2 marks an ambitious task (40xp) as undone
                 # They go back to level 1 (100xp threshold), but with 80xp already attained
-                user.exp_points = user.xp_threshold - current_xp
+                # First, deduct the user's xp from the xp value of the task being marked as not done
+                leftover = xp - current_xp
+                # Then set the user's xp as the lower level xp_threshold minus this leftover xp
+                # This gives the desired effect
+                user.exp_points = user.xp_threshold - leftover
                 
                 # Feedback to the user their level loss
                 messages.warning(request, 'You lost a level!')
-            
-        # If the user's xp has reached or exceeded their current xp_threshold
-        if user.exp_points >= user.xp_threshold:
-            # Reset experience points, but allow the user to retain full xp from a task
-            # e.g. if a user completes an amibitous task (40xp) when they only need 10 to level up
-            # they will have 30xp towards the next level
-            user.exp_points = 0 + leftover
-            # Increment the users level by 1
-            user.level_rank +=1
-            # Increment the total levels gained on the app by 1
-            stats.total_levels_gain += 1
-            # Set the new xp_threshold
-            # Each subsequent level gets harder to obtain!
-            user.xp_threshold += base_xp_threshold
-            
-            # Feedback to the user
-            messages.success(request, "Well done! You've just gained a level!")
-    
           
         # Save the updated user  
         user.save()
