545a8c0f9aef47fce40cdae91919170f717c527c
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-05-17 12:08:14 +0000

    Passed test test_get_stats_page

--- levelup/tests/test_views.py
+++ levelup/tests/test_views.py
@@ -20,6 +20,7 @@ class TestViews(TestCase):
         """
         Simple url test, ensure that the home page can be reached
         """
+        # Go to the url
         page = self.client.get("/")
         
         """ Assert that the home page can be reached and that the template used is index.html """
@@ -30,6 +31,7 @@ class TestViews(TestCase):
         """
         Ensure that the signup page can be reached
         """
+        # Go to the url
         page = self.client.get('/sign_up/')
         
         """ Assert that the signup page can be reached and that the template used is signup.html """
@@ -40,23 +42,44 @@ class TestViews(TestCase):
         """
         Ensure that the login page can be reached
         """
+        # Go to the url
         page = self.client.get('/login/')
         
         """ Assert that the login page can be reached and that the template used is login.html """
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "accounts/login.html")    
-    
+        
+    def test_get_stats_page(self):
+        """
+        Ensure that the stats page can be reached
+        """
+        # Unless an instance of StatsModel exists, navigating to /stats/ throws a 404 error
+        stats = StatsModel.objects.create(stats_name = 'Totals')
+        
+        # Go to the url
+        page = self.client.get('/stats/')
+        
+        """ Assert that the stats page can be reached and that the template used is stats.html """
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "stats/stats.html")    
+        
     def test_cannot_get_tasks_page_without_login(self):
         """
-        Since there is no authenticated user in the class yet, trying to get to the tasks page should get a 302 code, as a redirect
+        Since there is no authenticated user in the class, trying to get to the tasks page should get a 302 code, as a redirect
         to login should occur
         """
+        # Go to the url
         page = self.client.get("/tasks/")
+        
+        """ Assert that a redirection occurs """
         self.assertEqual(page.status_code, 302)
         
     def test_get_404_page_for_url_that_doesnt_exist(self):
         """
         Test that the 404 page appears when it should
         """
+        # Go to a fake url
         page = self.client.get('/blahfooblahfooblah')
+        
+        """ Assert that a 404 error is thrown """
         self.assertEqual(page.status_code, 404)
