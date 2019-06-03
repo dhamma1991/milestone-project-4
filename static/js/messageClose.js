3ec349a82ec4ee0be2540d6c5576c817779e5874
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-06-03 10:53:40 +0000

    Changed Django messages close effect to be a slideUp

--- static/js/messageClose.js
+++ static/js/messageClose.js
@@ -1,5 +1,12 @@
+/*
+
+This script allows the messages passed through from Django to the frontend
+to be closed when the user clicks the 'x' icon
+
+*/
+
 $(document).ready(function() {
     $('#message-close-btn').click(function() {
-        $('.django-messages').hide();
+        $('.django-messages').slideUp(200);
     })
 })
